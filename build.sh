018fe46eccf39dd66e8d62dcd0876aeb9bce47db
hao <52927295@qq.com>
2023-09-21 22:25:38 +0800

    add ssh shell

--- build.sh
+++ build.sh
@@ -7,4 +7,7 @@ export CC=arm-linux-gnueabihf-gcc
 # 构建 Go 项目
 go build
 
+scp /home/hao/Desktop/workspace/cargo/cargo root@192.168.8.137:/usr/app/cargo
+ssh root@192.168.8.137 'cd /usr/app/ && exec bash -l'
+
 
