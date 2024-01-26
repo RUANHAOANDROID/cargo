6a8f474a9f779ac338321b8e66bc0aa6ee5326d3
hao88 <528927295@qq.com>
2024-01-26 17:45:10 +0800

    变更启动方式

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
+import "C"
 import (
 	"cargo/clib"
 	"cargo/msg"
 	"cargo/pkg"
 	"sync"
+	"time"
 )
 
 // main  -lpos -lm -lpng -lfontconfig -lfreetype -liconv
@@ -19,12 +21,26 @@ func main() {
 	display.LCDRow("C Test D", 8, 8, clib.DISP_FONT24)
 	display.LCDRow(pkg.NowTimeStr(), 8, 40, clib.DISP_FONT12)
 	display.LCDRow("-hao88.cloud", 80, 60, clib.DISP_FONT12)
-	wg.Add(1)
-	go clib.StartTcpServer(&msgChan)
-	go clib.StartC()
+	wg.Add(3)
+	go func() {
+		wg.Done()
+		clib.StartTcpServer(&msgChan)
+	}()
+	//go clib.StartC()
+	C.start_tcp()
+	time.Sleep(time.Second)
+	go func() {
+		wg.Done()
+		C.start_qr()
+	}()
+	go func() {
+		wg.Done()
+		C.ic_read()
+	}()
 	for msg := range msgChan {
 		pkg.Log.Println("msg chan->", msg.Type, msg.Content)
 	}
 	pkg.Log.Print("End......")
 	wg.Wait()
+	defer C.close_tcp_connection()
 }
