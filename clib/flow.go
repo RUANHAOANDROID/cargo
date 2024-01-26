31660ea9bf1be0a15db8017b73778fc81de9230a
hao88 <528927295@qq.com>
2024-01-26 18:31:55 +0800

    优化

--- clib/flow.go
+++ clib/flow.go
@@ -27,7 +27,7 @@ func process(conn net.Conn) {
 		pkg.Log.Println(string(buffer[1:]))
 		pkg.Log.Println(string(buffer[:1]))
 		display.LCDRow(string(buffer[1:]), 8, 40, DISP_FONT12)
-		pkg.APlay(pkg.SoundFiles[10])
+		pkg.APlay(pkg.SoundFiles[9])
 		//chanMsg <- msg.Message{Type: int(packet.Type), Content: packetContent}
 		//conn.Write([]byte(recvStr)) // 发送数据
 	}
