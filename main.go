b2fe6ac0f1f80aacf89b8e6c5c5431d959a2b95b
hao88 <528927295@qq.com>
2024-01-26 16:19:20 +0800

    指针传递

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"cargo/clib"
 	"cargo/msg"
 	"cargo/pkg"
+	"sync"
 )
 
 // main  -lpos -lm -lpng -lfontconfig -lfreetype -liconv
 func main() {
+	var wg sync.WaitGroup
 	pkg.Log.Print("Start......1")
 	msgChan := make(chan msg.Message)
 	display := clib.Display{}
@@ -17,10 +19,12 @@ func main() {
 	display.LCDRow("C Test D", 8, 8, clib.DISP_FONT24)
 	display.LCDRow(pkg.NowTimeStr(), 8, 40, clib.DISP_FONT12)
 	display.LCDRow("-hao88.cloud", 80, 60, clib.DISP_FONT12)
-	go clib.StartFlow(msgChan)
+	wg.Add(1)
+	go clib.StartFlow(&msgChan)
 	go clib.StartC()
 	for msg := range msgChan {
 		pkg.Log.Println("msg chan->", msg.Type, msg.Content)
 	}
 	pkg.Log.Print("End......")
+	wg.Wait()
 }
