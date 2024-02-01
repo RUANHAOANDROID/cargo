f1e2b6e6d1b87d9f20397c46e46dfafe388f9bdc
hao88 <528927295@qq.com>
2024-02-01 18:44:19 +0800

    ic test

--- api/icbc/icbc_test.go
+++ api/icbc/icbc_test.go
@@ -7,14 +7,37 @@ import (
 	"testing"
 )
 
-func TestCheckTicket(t *testing.T) {
+func loadConfig(t *testing.T) error {
 	configFile := filepath.Join("..", "..", "config.yml")
 	conf, err := config.Load(configFile)
 	if err != nil {
 		t.Error(err)
 	}
 	SetConfig(conf)
+	return err
+}
+func TestIDTicket(t *testing.T) {
+	err := loadConfig(t)
 	resp, err := CheckTicket("420325199211114511", ProtoID)
+	if err != nil {
+		fmt.Println(err.Error())
+		t.Error(err)
+	}
+	fmt.Println(resp)
+}
+
+func TestQRTicket(t *testing.T) {
+	err := loadConfig(t)
+	resp, err := CheckTicket("15943090529708<MjAwMDAwMTkyNDIwMjQtMDItMDEyMDI0LTAzLTAx>", ProtoQr)
+	if err != nil {
+		fmt.Println(err.Error())
+		t.Error(err)
+	}
+	fmt.Println(resp)
+}
+func TestICTicket(t *testing.T) {
+	err := loadConfig(t)
+	resp, err := CheckTicket("15943090529708<MjAwMDAwMTkyNDIwMjQtMDItMDEyMDI0LTAzLTAx>", ProtoQr)
 	if err != nil {
 		fmt.Println(err.Error())
 		t.Error(err)
