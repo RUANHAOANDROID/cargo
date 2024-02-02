182df30687c123ccacfdc56f3906e2d83c2bb794
hao88 <52927295@qq.com>
2024-02-02 15:20:57 +0800

    设置系统时间

---
+++ pkg/currenttime.go
@@ -0,0 +1,37 @@
+package pkg
+
+import (
+	"encoding/json"
+	"net/http"
+	"os/exec"
+)
+
+type SNTime struct {
+	SysTime1 string `json:"sysTime1"`
+	SysTime2 string `json:"sysTime2"`
+}
+
+func SetCurrentTime() {
+	clit := &http.Client{}
+	resp, err := clit.Get("https://quan.suning.com/getSysTime.do")
+	if err != nil {
+		Log.Println(err)
+		return
+	}
+	var snTime SNTime
+
+	err = json.NewDecoder(resp.Body).Decode(&snTime)
+	if err != nil {
+		Log.Println("JSON unmarshal error:", err)
+		return
+	}
+	Log.Println("sysTime1:", snTime.SysTime1)
+	Log.Println("sysTime2:", snTime.SysTime2)
+
+	cmd := exec.Command("date", "--set=", snTime.SysTime2)
+	cmd.Stderr = nil
+	if err := cmd.Start(); err != nil {
+		Log.Println("cmd run error:", err)
+		return
+	}
+}
