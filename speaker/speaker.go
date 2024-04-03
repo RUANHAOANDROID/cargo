100ecf74a38579e4c4ec5ea3be88a0675b07fa07
hao88 <52927295@qq.com>
2024-04-03 12:57:35 +0800

    播报非

--- speaker/speaker.go
+++ speaker/speaker.go
@@ -9,11 +9,11 @@ func Speaker(msg string, success bool) {
 	if success {
 		decaros.APlay("A")
 	} else {
-		if !strings.Contains(msg, "已验票") {
+		if strings.Contains(msg, "已验票") {
 			decaros.APlay("C")
 			return
 		}
-		if !strings.Contains(msg, "权限") {
+		if strings.Contains(msg, "权限") {
 			decaros.APlay("Z")
 			return
 		}
