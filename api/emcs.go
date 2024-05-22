191c4f297c0f3fa6bc80a5cd6b4ca7232c7375e2
hao88 <52927295@qq.com>
2024-05-22 16:49:23 +0800

    日志上传

--- api/emcs.go
+++ api/emcs.go
@@ -27,13 +27,13 @@ func SyncDayCount(count int32) {
 	}
 	data, err := proto.Marshal(upDayCOunt)
 	if err != nil {
-		pkg.Log.Fatalf("Failed to marshal example: %v", err)
+		pkg.Log.Println("Failed to marshal example: %v", err)
 	}
 
-	url := "http://emcs-api.youchiyun.com"
+	url := "http://emcs-api.youchiyun.com/gateMachine/syncDayCount"
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
 	if err != nil {
-		pkg.Log.Fatalf("Failed to create request: %v", err)
+		pkg.Log.Println("Failed to create request: %v", err)
 	}
 
 	req.Header.Set("Content-Type", "application/x-protobuf")
@@ -41,12 +41,12 @@ func SyncDayCount(count int32) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		pkg.Log.Fatalf("Failed to send request: %v", err)
+		pkg.Log.Println("Failed to send request: %v", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		pkg.Log.Fatalf("Bad response: %v", resp.Status)
+		pkg.Log.Println("Bad response: %v", resp.Status)
 	}
 	pkg.Log.Println("Request sent successfully!")
 }
@@ -76,13 +76,13 @@ func upLogs(upType string, requestContent string, responseContent string, status
 
 	data, err := proto.Marshal(apiLogRequest)
 	if err != nil {
-		pkg.Log.Fatalf("Failed to marshal example: %v", err)
+		pkg.Log.Println("Failed to marshal example: %v", err)
 	}
 
-	url := "http://emcs-api.youchiyun.com"
+	url := "http://emcs-api.youchiyun.com/gateMachine/apiLogUploadV2"
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
 	if err != nil {
-		pkg.Log.Fatalf("Failed to create request: %v", err)
+		pkg.Log.Println("Failed to create request: %v", err)
 	}
 
 	req.Header.Set("Content-Type", "application/x-protobuf")
@@ -90,13 +90,13 @@ func upLogs(upType string, requestContent string, responseContent string, status
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		pkg.Log.Fatalf("Failed to send request: %v", err)
+		pkg.Log.Println("Failed to send request: %v", err)
 	}
 	defer resp.Body.Close()
 
 	// 检查响应状态
 	if resp.StatusCode != http.StatusOK {
-		pkg.Log.Fatalf("Bad response: %v", resp.Status)
+		pkg.Log.Println("Bad response: %v", resp.Status)
 	}
 	pkg.Log.Println("Request sent successfully!")
 }
