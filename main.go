9a8217f642accf5117e429aa14d86b57147db13a
hao88 <52927295@qq.com>
2024-05-22 17:20:58 +0800

    发布等待

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func monitorResources() {
 }
 
 // 自启动至少需要10秒，等待系统组件加载完毕
-var sleepTime = 0 * time.Second
+var sleepTime = 10 * time.Second
 
 // main  -lpos -lm -lpng -lfontconfig -lfreetype -liconv
 func main() {
