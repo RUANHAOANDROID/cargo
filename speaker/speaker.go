fa85e199be5fc7cdf2255a6fe970c5ade1482d10
hao88 <52927295@qq.com>
2024-06-04 12:56:33 +0800

    异步

--- speaker/speaker.go
+++ speaker/speaker.go
@@ -41,8 +41,8 @@ func SpeakerGroup(number string) {
 	if num > 10 && num < 100 {
 		tens := num / 10  // 取十位
 		units := num % 10 // 取个位
-		decaros.APlay("tdp")
-		time.Sleep(1400 * time.Millisecond)
+		decaros.APlay("tp")
+		time.Sleep(1200 * time.Millisecond)
 		decaros.APlay(fmt.Sprintf("%d", tens))
 		time.Sleep(400 * time.Millisecond)
 		decaros.APlay("10")
