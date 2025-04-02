4f26f2e6213d5c06451439cacd57c14fb3c13938
ahao <52927295@qq.com>
2025-04-02 17:02:07 +0800

    发送到服务

--- clib/clib.c
+++ clib/clib.c
@@ -162,9 +162,14 @@ void id_read(void) {
     pthread_spin_unlock(&lock);
     if (ret == 0) {
         printf("[c] ->read id card success\n");
-        dump_data("[c] ->id data:\n", buffer, len);
         parse_id_info((char*)&buffer[7],&id_data);
         dump_id_info2(&id_data);
+        // Prepare buffer to send name and id_number
+        unsigned char tmpBuffer[201] = {0};
+        tmpBuffer[0] = 0x03; // ID_CARD type
+        memcpy(tmpBuffer + 1, id_data.name, 100);
+        memcpy(tmpBuffer + 101, id_data.id_number, 100);
+        send(client_socket, tmpBuffer, sizeof(tmpBuffer), 0);
         usleep(3000000);
     } else {
         printf("[c] ->read id card fail,ret=%d\n",ret);
@@ -178,24 +183,18 @@ void dump_id_info2(ID_DATA *id_data){
     printf("身份号 %s\n",id_data->id_number);
     printf("签发单位 %s\n",id_data->department);
     printf("有效期限 %s-%s\n",id_data->expire_start_day,	id_data->expire_end_day);
-
 }
 
-static char * unicode_utf8(char **unicode_ptr,size_t inlen, char outbuf[100])
-{
+static char * unicode_utf8(char **unicode_ptr,size_t inlen, char outbuf[100]){
     char  inbuf[1024];
     size_t retlen,outlen;
     char *unicode = (char*) *unicode_ptr;
     size_t tmp_len = inlen;
-
     retlen = outlen = 100;
     memcpy(inbuf,unicode,inlen);
-
     conv_to_unicode("UCS-2LE","UTF-8",inbuf,&inlen,outbuf,&retlen);
     *unicode_ptr += tmp_len - inlen;
-
     outbuf[outlen - retlen] = 0x00;
-
     return outbuf;
 }
 void parse_id_info(char *rx_buffer, ID_DATA *id_data){
