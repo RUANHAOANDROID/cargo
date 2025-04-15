fad5570c31796a1098b2e833dcc549879d97c7f1
ahao <52927295@qq.com>
2025-04-15 20:14:20 +0800

    5

--- clib/display.go
+++ clib/display.go
@@ -13,7 +13,7 @@ package clib
 #include "unistd.h"
 
 void ShowNetworkIcon(void){
-	LCD_DisplayIcon(ICON_SIGNAL,28);
+	LCD_DisplayIcon(ICON_SIGNAL,5);
 	//LCD_DisplayIcon(ICON_NETWORK,3);//网络类型
 	//LCD_DisplayIcon(ICON_WIFI,3);//wifi
 }
